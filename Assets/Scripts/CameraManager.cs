6a6e8bc1eb343032b65af90ce38721ff5f9e3407
John <johnbaraceros13@gmail.com>
2024-02-15 13:41:49 +1100

    Mouse for Camera Movement
    The mouse will now act as the camera movement around the character for better player advantage

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,20 +4,87 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Transform targetTransform; //the object the camera will follow
-    private Vector3 cameraFollowVelocity = Vector3.zero;
+    InputManager inputManager;
 
+    public Transform targetTransform; //the object the camera will follow
+    public Transform cameraPivot;
+    public Transform cameraTransform; //transform of the camera object
+    public LayerMask collisionLayers; //layers we want camera to collide with
+    private float defaultPosition;
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+    private Vector3 cameraVectorPosition;
+
+    public float cameraCollisionOffset = 0.2f; //how much the camera will jump off objects it's colliding with
+    public float minimumCollisionOffset = 0.2f;
+    public float cameraCollisionRadius = 0.2f;
     public float cameraFollowSpeed = 0.2f;
+    public float cameraLookSpeed = 2;
+    public float cameraPivotSpeed = 2;
+
+    public float lookAngle; //up and down
+    public float pivotAngle; //left and right
+    public float minimumPivotAngle = -25;
+    public float maximumPivotAngle = 35;
 
     private void Awake()
     {
+        inputManager = FindObjectOfType<InputManager>();
         targetTransform = FindObjectOfType<PlayerManager>().transform;
+        cameraTransform = Camera.main.transform;
+        defaultPosition = cameraTransform.localPosition.z;
     }
-    public void FollowTarget()
+
+    public void HandleAllCameraMovement()
+    {
+        FollowTarget();
+        RotateCamera();
+        HandleCameraCollisions();
+    }
+    private void FollowTarget()
     {
         Vector3 targetPosition = Vector3.SmoothDamp
             (transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
 
         transform.position = targetPosition;
+    }
+
+    private void RotateCamera()
+    {
+        lookAngle = lookAngle + (inputManager.cameraInputX * cameraLookSpeed);
+        pivotAngle = pivotAngle - (inputManager.cameraInputY * cameraPivotSpeed);
+        pivotAngle = Mathf.Clamp(pivotAngle, minimumPivotAngle, maximumPivotAngle);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = lookAngle;
+        Quaternion targetRotation = Quaternion.Euler(rotation);
+        transform.rotation = targetRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+        targetRotation = Quaternion.Euler(rotation);
+        cameraPivot.localRotation = targetRotation;
+    }
+
+    private void HandleCameraCollisions()
+    {
+        float targetPosition = defaultPosition;
+        RaycastHit hit;
+        Vector3 direction = cameraTransform.position - cameraPivot.position;
+        direction.Normalize();
+
+        if (Physics.SphereCast
+            (cameraPivot.transform.position, cameraCollisionRadius, direction, out hit, Mathf.Abs(targetPosition), collisionLayers))
+        {
+            float distance = Vector3.Distance(cameraPivot.position, hit.point);
+            targetPosition =- (distance - cameraCollisionOffset);
+        }
+
+        if (Mathf.Abs(targetPosition) < minimumCollisionOffset)
+        {
+            targetPosition = targetPosition - minimumCollisionOffset;
+        }
+
+        cameraVectorPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, 0.2f);
+        cameraTransform.localPosition = cameraVectorPosition;
     }
 }
